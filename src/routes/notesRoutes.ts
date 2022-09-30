20ba5ce1064480886af23c8a9dd9f4c8b54e9b8a
andrey21322 <74838841+andrey21322@users.noreply.github.com>
2022-09-30 12:12:27 +0300

    Update notesRoutes.ts

--- src/routes/notesRoutes.ts
+++ src/routes/notesRoutes.ts
@@ -2,41 +2,36 @@ import { Router } from "express"
 import * as noteService from '../services/note_service'
 
 const router = Router()
-const {addNewPost, getAllPosts, deletePost, updatePost, getStats, getPost} = noteService.default
+const {addNewPost, getAllPosts, deletePost, updatePost, getPost} = noteService.default
 
 router.get('/', async (req, res) => {
     res.send(await getAllPosts())
 })
 
-router.get('/post/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     const id = req.params.id
     let post = await getPost(id)
     res.send(post)
 })
 
-router.get('/stats', async (req, res) => {
-    const stats = await getStats()
-    res.send(`notes count: ${stats}`)
-})
-
-router.delete('/delete/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     const id = req.params.id
     let response = await deletePost(id.toString())
     res.send(response)
 })
 
-router.post('/add', async (req, res) => {
+router.post('/', async (req, res) => {
     const post = req.body
     let response = await addNewPost(post)
     res.send(response)
 })
 
-router.patch('/edit/:id', async (req, res) => {
+router.patch('/:id', async (req, res) => {
     const id = req.params.id
     const post = req.body
     post.id = id
     await updatePost(post)
-    res.redirect('/posts')
+    res.redirect('/notes')
 })
 
 export default router
